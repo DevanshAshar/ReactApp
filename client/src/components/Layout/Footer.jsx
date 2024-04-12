7f49ea76c52450685dcc5269f28cab16ec658c71
Devansh Ashar <ashar.devansh@gmail.com>
2024-04-12 18:41:00 +0530

    updated footer

--- client/src/components/Layout/Footer.jsx
+++ client/src/components/Layout/Footer.jsx
@@ -1,20 +1,33 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
-    <footer style={{ backgroundColor:'#333',padding: '2rem', display: 'grid', gridTemplateColumns: 'repeat(3,1fr)', placeItems: 'center' }}>
-        <div>
-          <h3 style={{ fontWeight: 'bold' }}>Links</h3>
-          <div style={{ color: '#af99ff' }}>
-            <a href='/top'>Home</a>
-          </div>
+    <footer
+      style={{
+        backgroundColor: "#333",
+        padding: "2rem",
+        display: "grid",
+        gridTemplateColumns: "repeat(3,1fr)",
+        placeItems: "center",
+      }}
+    >
+      <div>
+        <h3 style={{ fontWeight: "bold", color: "white" }}>Links</h3>
+        <div
+          style={{ color: "#af99ff", display: "flex", flexDirection: "column" }}
+        >
+          <a href="/">Home</a>
+          <a href="/about">About Us</a>
         </div>
-        <div>
-          <a href='/#top'>
-            <img src='/logo.png' alt='JobSeeker Logo' height='64rem' />
-          </a>
-          <h3 style={{ fontWeight: 'bold' }}>JobSeeker &copy; {new Date().getFullYear()}</h3>
-        </div>
-      </footer>
-  )
+      </div>
+      <div>
+        <a href="/#top">
+          <img src="/logo.png" alt="JobSeeker Logo" height="64rem" />
+        </a>
+        <h3 style={{ fontWeight: "bold", color: "white" }}>
+          JobSeeker &copy; {new Date().getFullYear()}
+        </h3>
+      </div>
+    </footer>
+  );
 }
